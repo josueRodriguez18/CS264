689349360658a297169b2911829f3f48894a24d4
josueRodriguez18 <josue.a.rodriguez@ttu.edu>
2018-07-10 12:33:34 +0100

    added LAB3, started first cpp

---
+++ LAB3/Sequence1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main(){
+	int size;
+	
+	cout << "Please input the size of the sequence: ";
+	
+	cin >> size;
+	
+	double *p = new double[size];
+	
+	cout << "\n Please input your sequence elements now: ";
+	
+	for(int i = 0; i < size; i++){
+		cin >> p[i];
+	}
+	
+	for(int i = 0; i < size; i++){
+		
+		cout << endl << p[i];
+	}
+	
+
+
+	return 0;
+
+}
